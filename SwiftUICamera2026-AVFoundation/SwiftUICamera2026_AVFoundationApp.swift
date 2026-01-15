0c6cfbe2ad5fcd1bf708d27e112b9ef03fbaad0b
NDC Swift <noah_carpenter@hotmail.com>
2026-01-15 12:56:02 -0500

    Initial Commit

---
+++ SwiftUICamera2026-AVFoundation/SwiftUICamera2026_AVFoundationApp.swift
@@ -0,0 +1,22 @@
+//
+    // Project: SwiftUICamera2026-AVFoundation
+    //  File: SwiftUICamera2026_AVFoundationApp.swift
+//  Created by Noah Carpenter
+//  🐱 Follow me on YouTube! 🎥
+//  https://www.youtube.com/@NoahDoesCoding97
+//  Like and Subscribe for coding tutorials and fun! 💻✨
+//  Fun Fact: Cats have five toes on their front paws, but only four on their back paws! 🐾
+//  Dream Big, Code Bigger
+
+    
+
+import SwiftUI
+
+@main
+struct SwiftUICamera2026_AVFoundationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
